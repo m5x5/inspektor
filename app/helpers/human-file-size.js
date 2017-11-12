674aff63cf63e75565afdb8090f1110fcb2c7555
Sebastian Kippe <sebastian@kip.pe>
2017-11-13 00:11:23 +0100

    Show file sizes in listings

---
+++ app/helpers/human-file-size.js
@@ -0,0 +1,16 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function(fileSizeInBytes/*, options*/) {
+  if (fileSizeInBytes < 1024) {
+    return `${fileSizeInBytes} bytes`;
+  }
+
+  var i = -1;
+  var byteUnits = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  do {
+      fileSizeInBytes = fileSizeInBytes / 1024;
+      i++;
+  } while (fileSizeInBytes > 1024);
+
+  return String(Math.max(fileSizeInBytes, 0.1).toFixed(1) + ' ' +byteUnits[i]);
+});
