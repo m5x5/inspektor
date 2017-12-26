0a41be32fd2196625f1e63576b969c0508a229cd
Sebastian Kippe <sebastian@kip.pe>
2017-12-26 12:03:32 +0100

    Fix route test

--- tests/unit/routes/connect-test.js
+++ tests/unit/routes/connect-test.js
@@ -1,8 +1,7 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:connect', 'Unit | Route | connect', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:storage']
 });
 
 test('it exists', function(assert) {
